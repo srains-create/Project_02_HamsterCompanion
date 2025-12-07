b424cd925b4bffac819e674995dbba235e3762b0
sylvia-ww <168684863+sylvia-ww@users.noreply.github.com>
2025-12-07 00:21:32 -0800

    Created Hamster entity

---
+++ app/src/main/java/com/example/project_02_hamstercompanion/database/entities/Hamster.java
@@ -0,0 +1,112 @@
+package com.example.project_02_hamstercompanion.database.entities;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity(tableName = "hamsterTable")
+public class Hamster {
+    @PrimaryKey(autoGenerate = true)
+    private int hamsterId;
+
+    private int userId;
+    private String name;
+    private int hunger;
+    private int cleanliness;
+    private int energy;
+    private LocalDateTime adoptionDate;
+
+    public Hamster(int userId, String name, int hunger, int cleanliness, int energy) {
+        this.userId = userId;
+        this.name = name;
+        this.hunger = hunger;
+        this.cleanliness = cleanliness;
+        this.energy = energy;
+        this.adoptionDate = LocalDateTime.now();
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Hamster{" +
+                "adoptionDate=" + adoptionDate +
+                ", hamsterId=" + hamsterId +
+                ", userId=" + userId +
+                ", name='" + name + '\'' +
+                ", hunger=" + hunger +
+                ", cleanliness=" + cleanliness +
+                ", energy=" + energy +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Hamster)) return false;
+        Hamster hamster = (Hamster) o;
+        return hamsterId == hamster.hamsterId && userId == hamster.userId && hunger == hamster.hunger && cleanliness == hamster.cleanliness && energy == hamster.energy && Objects.equals(name, hamster.name) && Objects.equals(adoptionDate, hamster.adoptionDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(hamsterId, userId, name, hunger, cleanliness, energy, adoptionDate);
+    }
+
+    public int getHamsterId() {
+        return hamsterId;
+    }
+
+    public void setHamsterId(int hamsterId) {
+        this.hamsterId = hamsterId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHunger() {
+        return hunger;
+    }
+
+    public void setHunger(int hunger) {
+        this.hunger = hunger;
+    }
+
+    public int getCleanliness() {
+        return cleanliness;
+    }
+
+    public void setCleanliness(int cleanliness) {
+        this.cleanliness = cleanliness;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public LocalDateTime getAdoptionDate() {
+        return adoptionDate;
+    }
+
+    public void setAdoptionDate(LocalDateTime adoptionDate) {
+        this.adoptionDate = adoptionDate;
+    }
+}
