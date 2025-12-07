ab0e9483c7ef10e7b38dde7f13c8ba5aa105c4d2
sylvia-ww <168684863+sylvia-ww@users.noreply.github.com>
2025-12-07 00:44:27 -0800

    Added imports. changed Hamster entity so that no adoption date = not adopted

--- app/src/main/java/com/example/project_02_hamstercompanion/HamsterDatabase.java
+++ app/src/main/java/com/example/project_02_hamstercompanion/HamsterDatabase.java
@@ -1,4 +1,6 @@
 package com.example.project_02_hamstercompanion;
 
 public class HamsterDatabase {
+    public static final String USER_TABLE = "userTable";
+    public static final String HAMSTER_TABLE = "hamsterTable";
 }
