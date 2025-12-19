3970bd66bf0ad221dbacddfec6dab036d8c58f16
sylvia-ww <168684863+sylvia-ww@users.noreply.github.com>
2025-12-18 19:17:33 -0800

    Created CareLogEntityTest with unit test careLog_hasCorrectFields

---
+++ app/src/androidTest/java/com/example/project_02_hamstercompanion/database/entities/CareLogEntityTest.java
@@ -0,0 +1,24 @@
+package com.example.project_02_hamstercompanion.database.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CareLogEntityTest {
+
+    @Test
+    public void careLog_hasCorrectFields(){
+        int hamsterId = 1;
+        String action = "rest";
+        long timestamp = System.currentTimeMillis();
+        String toastNote = "Hamster rested.";
+        CareLog careLog = new CareLog(hamsterId, action, timestamp, toastNote);
+
+        assertEquals(hamsterId, careLog.getHamsterId());
+        assertEquals(action, careLog.getActionType());
+        assertEquals(timestamp, careLog.getTimestamp());
+        assertEquals(toastNote, careLog.getNotes());
+    }
+}
