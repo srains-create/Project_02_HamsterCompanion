366b51810e7b10dc90e373b0e3bf4918f6e3b433
srains <srains@csumb.edu>
2025-12-18 17:59:25 -0800

    implement HamsterDetailActivity stats screen and insert care log entries on actions

--- app/src/main/java/com/example/project_02_hamstercompanion/HamsterDetailActivity.java
+++ app/src/main/java/com/example/project_02_hamstercompanion/HamsterDetailActivity.java
@@ -8,20 +8,28 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 
 import com.example.project_02_hamstercompanion.database.HamsterRepository;
 import com.example.project_02_hamstercompanion.database.entities.CareLog;
 import com.example.project_02_hamstercompanion.database.entities.Hamster;
+import com.example.project_02_hamstercompanion.databinding.ActivityHamsterDetailBinding;
+import com.example.project_02_hamstercompanion.databinding.ActivityHamsterHomeBinding;
+import com.example.project_02_hamstercompanion.viewHolders.CareLogViewModel;
 
 
 public class HamsterDetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_HAMSTER_ID =
             "com.example.project_02_hamstercompanion.EXTRA_HAMSTER_ID";
+    private static final String HAMSTER_ID_KEY = "HAMSTER_ID";
+    private static final String USER_ID_KEY = "USER_ID";
+    private static final String USERNAME_KEY = "USERNAME";
 
     private TextView textHamsterName;
     private TextView textHungerValue;
@@ -36,17 +44,48 @@ public class HamsterDetailActivity extends AppCompatActivity {
     private HamsterRepository repository;
     private Hamster currentHamster;
 
+    private @NonNull ActivityHamsterDetailBinding binding;
+    private CareLogViewModel careLogViewModel;
+    private int hamsterId;
+    private int userId;
+    private String username;
 
     public static Intent intentFactory(Context context, int hamsterId) {
-        Intent intent = new Intent(context, HamsterDetailActivity.class);
-        intent.putExtra(EXTRA_HAMSTER_ID, hamsterId);
-        return intent;
+        return null;
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hamster_detail);
+
+        binding= ActivityHamsterDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        userId = getIntent().getIntExtra(HamsterDetailActivity.USER_ID_KEY,-1);
+        username = getIntent().getStringExtra(HamsterDetailActivity.USERNAME_KEY);
+        hamsterId = getIntent().getIntExtra(HamsterDetailActivity.HAMSTER_ID_KEY, -1);
+
+        careLogViewModel = new ViewModelProvider(this)
+                .get(CareLogViewModel.class);
+
+        binding.buttonFeed.setOnClickListener(v -> {
+           addCareLogEntry("FEED");
+        });
+
+        binding.buttonClean.setOnClickListener(v -> {
+            addCareLogEntry("CLEAN");
+        });
+
+        binding.buttonPlay.setOnClickListener(v -> {
+            addCareLogEntry("PLAY");
+        });
+
+        binding.buttonRest.setOnClickListener(v -> {
+            addCareLogEntry("REST");
+        });
+
+
 
         initViews();
 
@@ -106,7 +145,7 @@ public class HamsterDetailActivity extends AppCompatActivity {
     }
 
     private void loadHamster(int hamsterId) {
-        loadHamster(hamsterId);
+    //    loadHamster(hamsterId);
         LiveData<Hamster> hamsterLiveData = repository.getHamsterById(hamsterId);
         hamsterLiveData.observe(this, new Observer<Hamster>() {
             @Override
@@ -233,5 +272,23 @@ public class HamsterDetailActivity extends AppCompatActivity {
         repository.insertCareLog(log);
 
         updateUiFromHamster();
+    }
+
+    private void addCareLogEntry(String action){
+        int hunger = 0;
+        int cleanliness = 0;
+        int energy = 0;
+
+        CareLog log = new CareLog(hamsterId, hunger, cleanliness, energy);
+        careLogViewModel.addCareLog(log);
+    }
+
+    public static Intent intentFactory(Context context, int hamsterId, int userId, String username) {
+        Intent intent = new Intent(context, HamsterDetailActivity.class);
+        intent.putExtra(EXTRA_HAMSTER_ID, hamsterId);
+        intent.putExtra(HAMSTER_ID_KEY, hamsterId);
+        intent.putExtra(USER_ID_KEY, userId);
+        intent.putExtra(USERNAME_KEY, username);
+        return intent;
     }
 }
